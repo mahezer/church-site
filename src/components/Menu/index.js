75a8d6884788871a4e1be9cac3b38c45854ec509
Mahezer Carvalho <mahezercarvalho@Mahezers-MacBook-Air.local>
2023-02-23 20:59:38 -0300

    Create AppBar component and add Carousel

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,13 +1,26 @@
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { Link } from "react-router-dom";
 
 import logo from '../../assets/logopib.png';
 
-const MainMenu = () => (<Menu open>
-  <Link to="/">
-    <img src={logo} alt="logo" width="200px" />
-  </Link>
-</Menu>);
+const menuTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#fafafa',
+    },
+  },
+});
+
+const MainMenu = () => (<ThemeProvider theme={menuTheme}>
+    <AppBar position="static">
+    <Toolbar>
+      <Link to="/">
+        <img src={logo} alt="logo" width="200px" />
+      </Link>
+    </Toolbar>
+  </AppBar>
+</ThemeProvider>);
 
 export default MainMenu;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,41 @@
 import Page from '../components/Page';
 import Menu from '../components/Menu';
+import Carousel from 'react-material-ui-carousel';
+import { Paper, Button } from '@mui/material'
+
+const items = [
+  {
+    name: "Random Name #1",
+    description: "Probably the most random thing you have ever seen!"
+  },
+  {
+    name: "Random Name #2",
+    description: "Hello World!"
+  }
+];
+
+const Item = (props) => (
+  <Paper>
+    <h2>{props.item.name}</h2>
+    <p>{props.item.description}</p>
+    <Button className="CheckButton">
+      Check it out!
+    </Button>
+  </Paper>
+)
+
 
 function Home() {
   return (
     <div>
       <Menu />
-      <Page />
+        <div>
+          <Carousel>
+            { items.map( (item, i) => <Item key={i} item={item} /> ) }
+          </Carousel>
+        </div>
     </div>
-  );
+    );
 }
 
 export default Home;
