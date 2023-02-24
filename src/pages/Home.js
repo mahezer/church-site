2de4d11aa3441bbbdf28bf6042d2d895164e3fbc
Otávio Fernandes <otaviofernandes095@outlook.com>
2023-02-23 22:11:47 -0200

    Create Table on Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,14 @@ import Page from '../components/Page';
 import Menu from '../components/Menu';
 import Carousel from 'react-material-ui-carousel';
 import { Paper, Button } from '@mui/material'
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 
 const items = [
   {
@@ -29,11 +37,30 @@ function Home() {
   return (
     <div>
       <Menu />
-        <div>
-          <Carousel>
-            { items.map( (item, i) => <Item key={i} item={item} /> ) }
-          </Carousel>
-        </div>
+      <div id='carousel'>
+        <Carousel>
+          { items.map( (item, i) => <Item key={i} item={item} /> ) }
+        </Carousel>
+      </div>
+      <Grid container>
+        <Grid item xs={12}>
+          <Grid container justifyContent="center">
+            <Typography  component="h2">
+              NOSSA SEMANA
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container justifyContent="center">
+            <TableContainer>
+              <Table>
+                <TableHead></TableHead>
+                <TableBody></TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
     );
 }
